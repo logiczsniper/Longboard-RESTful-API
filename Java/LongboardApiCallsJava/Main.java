3e200ed90fafa3d5f27455ca68e9b20f6f98077a
Logan <lczernel@gmail.com>
2018-11-01 21:44:36 +0000

    Massive progress in Main.java
    Created a very large method, addElements(), to create all the buttons
    and labels for each section. A section, in this context, is defined as
    a group of similar requests. E.g,  GET a specific longboard and GET all
    longboards would be in the same section. Overall, this reduces the
    redundancy of the code. The method has documentation.
    
    In addition, the first three sections (GET, DELETE and OPTIONS) have
    been added using the new method. The ActionListeners associated with all
    the buttons however, are still placeholders; they dont actually carry
    out their intended functions yet.
    
    Lastly, the button sound has been fixed- it now plays successfully each
    button press. However, the slight lag of the initial sound has returned.

--- Java/LongboardApiCallsJava/Main.java
+++ Java/LongboardApiCallsJava/Main.java
@@ -6,8 +6,11 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
@@ -36,18 +39,16 @@ public class Main {
         Image icon = Toolkit.getDefaultToolkit().getImage("static/icon.png");
         appScreen.setIconImage(icon);
 
-        Clip sound = loadClipAttempt();
-
         // TODO: background music
 
-        /**
-         *         LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
-         *         new_args.put("id", "003");
-         *         new_args.put("name", "Board from JAVA two");
-         *         new_args.put("length", "1");
-         *         new_args.put("width", "25");
-         *
-         *         LongboardApiRequests.postLongboard(new_args);
+        /* For reference when creating specific api calls in the ActionLister(s).
+
+         LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
+         new_args.put("id", "003");
+         new_args.put("name", "Board from JAVA two");
+         new_args.put("length", "1");
+         new_args.put("width", "25");
+         LongboardApiRequests.postLongboard(new_args);
 
          LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
          new_args.put("name", "Board from JAVA");
@@ -62,31 +63,95 @@ public class Main {
          System.out.println(LongboardApiRequests.simpleLongboardRequest("get"));
          */
 
-        // Get a longboard from a id
-        JLabel idLabel = new JLabel("ID: ");
-        idLabel.setBounds(20, 20, 80, 30);
-        appScreen.add(idLabel);
+        // Adding get panels
+        addElements(appScreen, new ArrayList<>(Arrays.asList(40, 60, 80, 30)),
+                new ArrayList<>(Arrays.asList(100, 60, 200, 30)), "Get Longboard",
+                new ArrayList<>(Arrays.asList(100, 100, 200, 30)), e -> {
+                    try {
 
-        final JTextField userInput = new JTextField();
-        userInput.setBounds(100, 20, 200, 30);
-        appScreen.add(userInput);
+                        playSoundAttempt();
 
-        JButton getLongboardButton = new JButton("Get Longboard");
-        getLongboardButton.setBounds(100, 60, 200, 30);
-        getLongboardButton.addActionListener(e -> {
-            try {
-                assert sound != null;
-                // TODO: button sound only plays once-- FIX
-                sound.start();
+                        // TODO: use api here
 
-                // TODO: use api here
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                }, "Get All Longboards", new ArrayList<>(Arrays.asList(100, 140, 200, 30)),
+                e -> {
+                    try {
 
-                JOptionPane.showMessageDialog(appScreen, "");
-            } catch (Exception el) {
-                el.printStackTrace();
-            }
-        });
-        appScreen.add(getLongboardButton);
+                        playSoundAttempt();
+
+                        // TODO: use api here
+
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                });
+
+        // Adding deletion panels
+        addElements(appScreen, new ArrayList<>(Arrays.asList(40, 240, 80, 30)),
+                new ArrayList<>(Arrays.asList(100, 240, 200, 30)), "Delete Longboard",
+                new ArrayList<>(Arrays.asList(100, 280, 200, 30)), e -> {
+                    try {
+
+                        playSoundAttempt();
+
+                        // TODO: use api here
+
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                }, "Delete All Longboards", new ArrayList<>(Arrays.asList(100, 320, 200, 30)),
+                e -> {
+                    try {
+
+                        playSoundAttempt();
+
+                        // TODO: use api here
+
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                });
+
+        // Adding options panels
+        addElements(appScreen, new ArrayList<>(Arrays.asList(40, 420, 80, 30)),
+                new ArrayList<>(Arrays.asList(100, 420, 200, 30)), "Options Longboard",
+                new ArrayList<>(Arrays.asList(100, 460, 200, 30)), e -> {
+                    try {
+
+                        playSoundAttempt();
+
+                        // TODO: use api here
+
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                }, "Options All Longboards", new ArrayList<>(Arrays.asList(100, 500, 200, 30)),
+                e -> {
+                    try {
+
+                        playSoundAttempt();
+
+                        // TODO: use api here
+
+                        JOptionPane.showMessageDialog(appScreen, "");
+                    } catch (Exception el) {
+                        el.printStackTrace();
+                    }
+                });
+
+        // Adding post panels
+        // TODO: addElements call for post
+
+        // Adding put/patch panels
+        // TODO: addElements call for put/patch
 
         // Finish setting up application
         appScreen.setSize(935, 645);
@@ -95,19 +160,58 @@ public class Main {
 
     }
 
+
+    // TODO: create method that takes JSON string and converts it to a pretty string to be displayed by the popup
+
+
     /**
-     * Attempts to load the button sound and handles all exceptions.
+     * Creates elements and adds them to the screen using the many arguments given.
+     *
+     * @param appScreen      the JFrame of the app that the elements will be added to.
+     * @param posOne         the x, y, width and height of the JLabel.
+     * @param posTwo         the x, y, width and height of the JTextField.
+     * @param buttonOneLabel the text that will be put onto the first JButton.
+     * @param posThree       the x, y, width and height of the JButton.
+     * @param operatorOne    the ActionListener that will be applied to the first JButton.
+     * @param buttonTwoLabel the text that will be put onto the second JButton.
+     * @param posFour        the x, y, width and height of the JButton.
+     * @param operatorTwo    the ActionListener that will be applied to the second JButton.
      */
-    private static Clip loadClipAttempt() {
+    private static void addElements(JFrame appScreen, ArrayList<Integer> posOne, ArrayList<Integer> posTwo,
+                                    String buttonOneLabel, ArrayList<Integer> posThree, ActionListener operatorOne,
+                                    String buttonTwoLabel, ArrayList<Integer> posFour, ActionListener operatorTwo) {
+
+        JLabel idLabel = new JLabel("ID: ");
+        idLabel.setBounds(posOne.get(0), posOne.get(1), posOne.get(2), posOne.get(3));
+        appScreen.add(idLabel);
+
+        JTextField userInput = new JTextField();
+        userInput.setBounds(posTwo.get(0), posTwo.get(1), posTwo.get(2), posTwo.get(3));
+        appScreen.add(userInput);
+
+        JButton specificLongboardButton = new JButton(buttonOneLabel);
+        specificLongboardButton.setBounds(posThree.get(0), posThree.get(1), posThree.get(2), posThree.get(3));
+        specificLongboardButton.addActionListener(operatorOne);
+        appScreen.add(specificLongboardButton);
+
+        JButton generalLongboardsButton = new JButton(buttonTwoLabel);
+        generalLongboardsButton.setBounds(posFour.get(0), posFour.get(1), posFour.get(2), posFour.get(3));
+        generalLongboardsButton.addActionListener(operatorTwo);
+        appScreen.add(generalLongboardsButton);
+    }
+
+    /**
+     * Attempts to load and play the clip and handles all exceptions.
+     */
+    private static void playSoundAttempt() {
         try {
             String soundName = "static/button-sound.wav";
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-            return clip;
+            clip.start();
         } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ignored) {
         }
-        return null;
     }
 
 }
