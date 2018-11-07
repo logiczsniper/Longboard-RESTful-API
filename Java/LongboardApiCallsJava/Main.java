317b3fa810efece1b57769833a9cc0df6a4d586e
Logan <lczernel@gmail.com>
2018-11-07 17:07:12 +0000

    Near completed.
    Many changes and updates have been made.
    
    First, the panels for put, patch and post have been added and are
    functional. A new method, addComplexJButton, had been added for these
    new requests as they require more arguments than just the id of the
    longboard.
    Next, documentation has been added for every method that doesnt already
    have documentation as well as the new methods.
    Lastly, the multiline comment that was left from manual tests was used
    and some was transferred to the new method. Then it was removed for
    obvious reasons.
    Minor changes I wont go into detail about include declaring certain
    variables before initialzed for neatness and making soundName final.

--- Java/LongboardApiCallsJava/Main.java
+++ Java/LongboardApiCallsJava/Main.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 
 /**
@@ -41,46 +42,28 @@ public class Main {
         Image icon = Toolkit.getDefaultToolkit().getImage("static/icon.png");
         appScreen.setIconImage(icon);
 
-        /* For reference when creating specific api calls in the ActionLister(s).
-
-         LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
-         new_args.put("id", "003");
-         new_args.put("name", "Board from JAVA two");
-         new_args.put("length", "1");
-         new_args.put("width", "25");
-         LongboardApiRequests.postLongboard(new_args);
-
-         LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
-         new_args.put("name", "Board from JAVA");
-         new_args.put("length", "1");
-         new_args.put("width", "25");
-         SpecificLongboardApiRequests.putLongboard("001", new_args);
-
-         LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
-         new_args.put("length", "8");
-         SpecificLongboardApiRequests.patchLongboard("001", new_args);
-
-         System.out.println(LongboardApiRequests.simpleLongboardRequest("get"));
-         */
-
         // Add user input for simple requests
         JTextField simpleUserInput = addLabeledUserInput(appScreen, new ArrayList<>(Arrays.asList(100, 60, 200, 30)), "ID: ");
 
         // Add Longboard specific buttons
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 100, 200, 30)), "get", simpleUserInput, true);
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 140, 200, 30)), "delete", simpleUserInput, true);
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 180, 200, 30)), "options", simpleUserInput, true);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 100, 200, 30)), "get", simpleUserInput, true);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 140, 200, 30)), "delete", simpleUserInput, true);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 180, 200, 30)), "options", simpleUserInput, true);
 
         // Add General Longboard buttons
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 300, 200, 30)), "get", simpleUserInput, false);
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 340, 200, 30)), "delete", simpleUserInput, false);
-        addJButton(appScreen, new ArrayList<>(Arrays.asList(100, 380, 200, 30)), "options", simpleUserInput, false);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 300, 200, 30)), "get", simpleUserInput, false);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 340, 200, 30)), "delete", simpleUserInput, false);
+        addSimpleJButton(appScreen, new ArrayList<>(Arrays.asList(100, 380, 200, 30)), "options", simpleUserInput, false);
 
-        // Adding post panels
-        // TODO: call for post
+        // Adding post, put, patch panels
+        JTextField idUserInput = addLabeledUserInput(appScreen, new ArrayList<>(Arrays.asList(650, 60, 200, 30)), "ID: ");
+        JTextField nameUserInput = addLabeledUserInput(appScreen, new ArrayList<>(Arrays.asList(650, 100, 200, 30)), "NAME: ");
+        JTextField widthUserInput = addLabeledUserInput(appScreen, new ArrayList<>(Arrays.asList(650, 140, 200, 30)), "LENGTH: ");
+        JTextField lengthUserInput = addLabeledUserInput(appScreen, new ArrayList<>(Arrays.asList(650, 180, 200, 30)), "WIDTH: ");
 
-        // Adding put/patch panels
-        // TODO: call for put/patch
+        addComplexJButton(appScreen, new ArrayList<>(Arrays.asList(650, 220, 200, 30)), "post", idUserInput, nameUserInput, lengthUserInput, widthUserInput, false);
+        addComplexJButton(appScreen, new ArrayList<>(Arrays.asList(650, 260, 200, 30)), "put", idUserInput, nameUserInput, lengthUserInput, widthUserInput, true);
+        addComplexJButton(appScreen, new ArrayList<>(Arrays.asList(650, 300, 200, 30)), "patch", idUserInput, nameUserInput, lengthUserInput, widthUserInput, true);
 
         // Finish setting up application
         appScreen.setSize(935, 645);
@@ -89,10 +72,14 @@ public class Main {
 
     }
 
-    // TODO: create method that takes JSON string and converts it to a pretty string to be displayed by the popup
-
-    // TODO: documentation on new methods
-
+    /**
+     * Creates a user input and a label that corresponds to the input in terms of position and what should be provided.
+     *
+     * @param appScreen the app screen to add the new elements.
+     * @param position the x, y, width and height of the elements.
+     * @param label the text to become a JLabel object.
+     * @return the JTextField object created. This is done so that the content provided can be accessed.
+     */
     private static JTextField addLabeledUserInput(JFrame appScreen, ArrayList<Integer> position, String label) {
         JLabel idLabel = new JLabel(label);
         idLabel.setBounds(position.get(0) - 60, position.get(1), 80, 30);
@@ -105,19 +92,87 @@ public class Main {
         return userInput;
     }
 
-    private static void addJButton(JFrame appScreen, ArrayList<Integer> position, String method, JTextField simpleUserInput, Boolean isSpecific) {
+    /**
+     * Creates a button with a set up Action Listener.
+     *
+     * @param appScreen the app screen to add the new elements.
+     * @param position the x, y, width and height of the elements.
+     * @param method the HTTP request method that will be used as both a parameter to the Longboard Api request functions and also as
+     *               text for a label of the button.
+     * @param simpleUserInput the user input for longboard id. It is the only required parameter for some of the methods.
+     * @param isSpecific signals whether or not a Specific or regular request should be made. If specific, the id is passed to the
+     *                   specific longboard request function rather than no id being passed to the regular simple longboard request
+     *                   function.
+     */
+    private static void addSimpleJButton(JFrame appScreen, ArrayList<Integer> position, String method, JTextField simpleUserInput, Boolean isSpecific) {
         JButton button = new JButton(method.substring(0, 1).toUpperCase() + method.substring(1));
         button.setBounds(position.get(0), position.get(1), position.get(2), position.get(3));
         ActionListener baseListener = e -> {
             try {
 
                 playSoundAttempt();
+                String response;
 
                 if (isSpecific) {
-                    String response = SpecificLongboardApiRequests.simpleSpecificLongboardRequest(method, simpleUserInput.getText());
+                    response = SpecificLongboardApiRequests.simpleSpecificLongboardRequest(method, simpleUserInput.getText());
                     JOptionPane.showMessageDialog(appScreen, response);
                 } else {
-                    String response = LongboardApiRequests.simpleLongboardRequest(method);
+                    response = LongboardApiRequests.simpleLongboardRequest(method);
+                    JOptionPane.showMessageDialog(appScreen, response);
+                }
+
+            } catch (Exception el) {
+                el.printStackTrace();
+            }
+        };
+
+        button.addActionListener(baseListener);
+        appScreen.add(button);
+    }
+
+    /**
+     * Creates a button with a set up Action Listener. These Action Listeners, unlike those created by addSimpleJButton,
+     * make requests that are deemed 'complex' as they require more arguments than the 'simple' requests.
+     *
+     * @param appScreen the app screen to add the new elements.
+     * @param position the x, y, width and height of the elements.
+     * @param method the HTTP request method that will be used as both a parameter to the Longboard Api request functions and also as
+     *               text for a label of the button.
+     * @param idUserInput the user input that should contain the id to be used for the new longboard.
+     * @param nameUserInput the user input that should contain the name to be used for the new longboard.
+     * @param lengthUserInput the user input that should contain the length to be used for the new longboard.
+     * @param widthUserInput the user input that should contain the width to be used for the new longboard.
+     * @apiNote making a put request should only be made if all three of the optional parameters are provided. If they
+     * are not provided, a patch request should be used.
+     * @param isSpecific signals whether or not a Specific or regular request should be made. If specific, the id is passed to the
+     *                   specific longboard request function rather than no id being passed to the regular simple longboard request
+     *                   function.
+     */
+    private static void addComplexJButton(JFrame appScreen, ArrayList<Integer> position, String method,
+                                          JTextField idUserInput, JTextField nameUserInput, JTextField lengthUserInput,
+                                          JTextField widthUserInput, Boolean isSpecific) {
+        JButton button = new JButton(method.substring(0, 1).toUpperCase() + method.substring(1));
+        button.setBounds(position.get(0), position.get(1), position.get(2), position.get(3));
+        ActionListener baseListener = e -> {
+            try {
+
+                playSoundAttempt();
+                String response;
+
+                LinkedHashMap<String, String> new_args = new LinkedHashMap<>();
+                new_args.put("id", idUserInput.getText());
+                new_args.put("name", nameUserInput.getText());
+                new_args.put("length", lengthUserInput.getText());
+                new_args.put("width", widthUserInput.getText());
+
+                if (isSpecific) {
+
+                    response = SpecificLongboardApiRequests.complexSpecificLongboardRequest(method, idUserInput.getText(), new_args);
+                    JOptionPane.showMessageDialog(appScreen, response);
+
+                } else {
+
+                    response = LongboardApiRequests.postLongboard(new_args);
                     JOptionPane.showMessageDialog(appScreen, response);
                 }
 
@@ -135,7 +190,7 @@ public class Main {
      */
     private static void playSoundAttempt() {
         try {
-            String soundName = "static/button-sound.wav";
+            final String soundName = "static/button-sound.wav";
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
